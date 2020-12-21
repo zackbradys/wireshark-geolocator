be3f3307b1fd698e2411822f87e127f842dfc7a0
Zack Brady <zackbrady123@gmail.com>
2020-12-21 06:55:43 -0500

    Updated script (main.py)

--- main.py
+++ main.py
@@ -7,45 +7,37 @@ from geolite2 import geolite2
 import socket, subprocess
 
 # For wireless interface use, uncomment the line below
-# cmd = r"C:\Program Files\Wireshark\tshark.exe"
+cmd = r"C:\Program Files\Wireshark\tshark.exe -i Wi-Fi"
 
 # For wired interface use, comment the line below
-cmd = r"C:\Program Files\Wireshark\tshark.exe -i ethernet"
+# cmd = r"C:\Program Files\Wireshark\tshark.exe -i Ethernet"
 
 # Start Wireshark Geolocator Script
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 local_ip = socket.gethostbyname(socket.gethostname())
+
 reader = geolite2.reader()
 
-def determine_location(ip_address):
-    location = reader.get(ip_address)
+def determine_location(ip):
+    location = reader.get(ip)
     
     try:
-      continent = location["continent"]["names"]["en"]
+        country = location["country"]["names"]["en"]
     except:
-      continent = "Not found"
-      
-    try:
-      country = location["country"]["names"]["en"]
-    except:
-      country = "Not found"
+        country = "Indeterminable"
 
     try:
-      subdivision = location["subdivisions"][0]["names"]["en"]
+        subdivision = location["subdivisions"][0]["names"]["en"]
     except:
-      subdivision = "Not found"    
+        subdivision = "Indeterminable"    
 
     try:
-      city = location["city"]["names"]["en"]
+        city = location["city"]["names"]["en"]
     except:
-      city = "Not found"
+        city = "Indeterminable"
     
-    try:
-      postal = location["postal"]
-    except:
-      postal = "Not found"
-    
-    return continent, country, subdivision, city, postal
+    return country, subdivision, city
+
 
 for line in iter(process.stdout.readline, b""):
     columns = str(line).split(" ")
@@ -53,24 +45,24 @@ for line in iter(process.stdout.readline, b""):
     if "UDP" in columns or "SKYPE" in columns:
         
         if "->" in columns:
-            source_ip = columns[columns.index("->") - 1]
+            src_ip = columns[columns.index("->") - 1]
         elif "\\xe2\\x86\\x92" in columns:
-            source_ip = columns[columns.index("\\xe2\\x86\\x92") - 1]
+            src_ip = columns[columns.index("\\xe2\\x86\\x92") - 1]
         else:
             continue
             
-        if source_ip == local_ip:
+        if src_ip == local_ip:
             continue
 
         try:
-            country, subdivision, city = determine_location(source_ip)
-            print("Location Information:" + continent + " | " + country + " | " + subdivision + " | " + city + " | " + postal)
+            country, sub, city = determine_location(src_ip)
+            print(src_ip + " = " + "Location Info: " + country + ", " + sub + ", " + city)
         except:
             try:
-                true_ip = socket.gethostbyname(source_ip)
-                country, subdivision, city = determine_location(true_ip)
-                print("Location Information:" + continent + " | " + country + " | " + subdivision + " | " + city + " | " + postal)
+                true_ip = socket.gethostbyname(src_ip)
+                country, sub, city = determine_location(true_ip)
+                print(src_ip + " = " + "Location Info: " + country + ", " + sub + ", " + city)
             except:
-                print("Not found")
-                
+                print("Indeterminable")
+
 # End Wireshark Geolocator Script
