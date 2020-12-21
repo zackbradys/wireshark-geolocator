b87e499af67491cc4ca2ed9e85cd290ca8a2403a
Zack Brady <zackbrady123@gmail.com>
2020-12-20 23:39:11 -0500

    Updated comments on main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Wireshark Python Geolocator
+# Wireshark Geolocator
 # Simple python program using wireshark's command line interface to intercept users internet protocol addresses and determine their approximate location (IP Address are inherently imprecise). 
 # Once the IP Address has been determined via wireshark, this script will use MaxMind's GeoIP2 databases, to find their location information such as their country, state, and city. 
 # Once their IP Address and Location has been determined, it will print the result.
@@ -6,13 +6,13 @@
 from geolite2 import geolite2
 import socket, subprocess
 
-# For wireless interface use, uncomment below
+# For wireless interface use, uncomment the line below
 # cmd = r"C:\Program Files\Wireshark\tshark.exe"
 
-# For wired interface use, uncomment below
-# cmd = r"C:\Program Files\Wireshark\tshark.exe -i ethernet"
+# For wired interface use, comment the line below
+cmd = r"C:\Program Files\Wireshark\tshark.exe -i ethernet"
 
-
+# Start Wireshark Geolocator Script
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 local_ip = socket.gethostbyname(socket.gethostname())
 reader = geolite2.reader()
@@ -63,3 +63,5 @@ for line in iter(process.stdout.readline, b""):
                 print("Location Information:" + country + " | " + subdivision + " | " + city)
             except:
                 print("Not found.")
+                
+# End Wireshark Geolocator Script
